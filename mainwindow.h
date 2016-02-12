e740a3dfaaad356c091a98f33d58f111c08f5774
AiK <zheka824@gmail.com>
2016-02-12 14:44:04 +0300

    I got up, she got down Making her parents so proud

--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QEventLoop>
 #include <QUrl>
 #include <QThread>
 #include <QMessageBox>
@@ -14,6 +15,8 @@
 #include <QGraphicsScene>
 #include <QGraphicsSceneMouseEvent>
 #include <QCursor>
+#include <QFileDialog>
+#include <windows.h>
 
 
 
@@ -28,23 +31,33 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();    
-    void processRequest(int, int, int);
+    void processRequest();
+    void loadFourPictures();
+    void showCoo();
 
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *scene;
     QNetworkAccessManager *netManager;
-    QGraphicsPixmapItem *pixmapGraph;
+    QGraphicsItemGroup *pixmapGraph;
+    QGraphicsPixmapItem *gr1;
+    QGraphicsPixmapItem *gr2;
+    QGraphicsPixmapItem *gr3;
+    QGraphicsPixmapItem *gr4;
+    QEventLoop *eventloop;
     void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 
     int numberThread;
+    int numberPic;
+    int zoomLevel;
+    int x;
+    int y;
 
 
 public slots:
     void replyFinished(QNetworkReply* reply);
-
 
 };
 
